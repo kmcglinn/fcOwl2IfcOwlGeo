4998e491f61f89327554b85b82027c7986142aa3
Kris McGlinn <kmcglinn@gmail.com>
2019-06-07 16:34:24 +0100

    Second commit, licensing added

--- IfcOwl2IfcOwlGeo/src/main/java/tcd/OBJ.java
+++ IfcOwl2IfcOwlGeo/src/main/java/tcd/OBJ.java
@@ -20,6 +20,22 @@ import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.vocabulary.RDF;
 
+/*
+ * Copyright 2019 Joseph Donovan, Trinity College Dublin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class OBJ {
 	
     private String ns_ifcowl;
